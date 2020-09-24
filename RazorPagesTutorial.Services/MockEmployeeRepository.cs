5ba9ee3810fd8ef9a782a167915233513e62a77f
Zhining Zhang <zzhang@timios.com>
2020-09-23 17:20:37 -0700

    done part 6

---
+++ RazorPagesTutorial.Services/MockEmployeeRepository.cs
@@ -0,0 +1,30 @@
+﻿using RazorPagesTutorial.Models;
+using System.Collections.Generic;
+
+namespace RazorPagesTutorial.Services
+{
+    public class MockEmployeeRepository : IEmployeeRepository
+    {
+        private List<Employee> _employeeList;
+
+        public MockEmployeeRepository()
+        {
+            _employeeList = new List<Employee>()
+            {
+                new Employee() { Id = 1, Name = "Mary", Department = Dept.HR,
+                    Email = "mary@pragimtech.com", PhotoPath="mary.png" },
+                new Employee() { Id = 2, Name = "John", Department = Dept.IT,
+                    Email = "john@pragimtech.com", PhotoPath="john.png" },
+                new Employee() { Id = 3, Name = "Sara", Department = Dept.IT,
+                    Email = "sara@pragimtech.com", PhotoPath="sara.png" },
+                new Employee() { Id = 4, Name = "David", Department = Dept.Payroll,
+                    Email = "david@pragimtech.com" },
+            };
+        }
+
+        public IEnumerable<Employee> GetAllEmployees()
+        {
+            return _employeeList;
+        }
+    }
+}
